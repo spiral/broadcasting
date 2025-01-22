438477dbb3233e58423080d37efc21b7f852e5fb
spiralbot <ci-bot@spiralscout.com>
2025-01-22 15:13:32 +0000

    prepare release

--- tests/TopicRegistryTest.php
+++ tests/TopicRegistryTest.php
@@ -17,39 +17,53 @@ final class TopicRegistryTest extends TestCase
 
         $this->registry = new TopicRegistry([
             'bar-topic.{id}' => fn($id) => $id,
-            'foo-topic' => static fn(): string => 'foo',
+            'foo-topic' => fn() => 'foo',
         ]);
     }
 
     public function testRegisterTopic(): void
     {
-        $this->registry->register('baz-topic', static fn(): string => 'baz');
-        $this->registry->register('baz.{uuid}', static fn(string $uuid): string => $uuid);
+        $this->registry->register('baz-topic', fn() => 'baz');
+        $this->registry->register('baz.{uuid}', fn(string $uuid) => $uuid);
 
 
         $params = [];
-        self::assertSame('baz', call_user_func($this->registry->findCallback('baz-topic', $params)));
+        $this->assertSame(
+            'baz',
+            call_user_func($this->registry->findCallback('baz-topic', $params))
+        );
 
 
         $params = [];
-        self::assertSame('hello', call_user_func($this->registry->findCallback('baz.hello', $params), 'hello'));
-        self::assertSame([0 => 'baz.hello', 'uuid' => 'hello', 1 => 'hello'], $params);
+        $this->assertSame(
+            'hello',
+            call_user_func($this->registry->findCallback('baz.hello', $params), 'hello')
+        );
+        $this->assertSame([0 => 'baz.hello', 'uuid' => 'hello', 1 => 'hello'], $params);
     }
 
     public function testFindsTopicCallback(): void
     {
         $params = [];
-        self::assertSame('foo', call_user_func($this->registry->findCallback('foo-topic', $params)));
+        $this->assertSame(
+            'foo',
+            call_user_func($this->registry->findCallback('foo-topic', $params))
+        );
 
-        self::assertSame([0 => 'foo-topic'], $params);
-
-        $params = [];
-        self::assertSame(5, call_user_func($this->registry->findCallback('bar-topic.5', $params), 5));
-        self::assertSame([0 => 'bar-topic.5', 'id' => '5', 1 => '5'], $params);
-
+        $this->assertSame([0 => 'foo-topic'], $params);
 
         $params = [];
-        self::assertNull($this->registry->findCallback('baz-topic', $params));
-        self::assertSame([], $params);
+        $this->assertSame(
+            5,
+            call_user_func($this->registry->findCallback('bar-topic.5', $params), 5)
+        );
+        $this->assertSame([0 => 'bar-topic.5', 'id' => '5', 1 => '5'], $params);
+
+
+        $params = [];
+        $this->assertNull(
+            $this->registry->findCallback('baz-topic', $params)
+        );
+        $this->assertSame([], $params);
     }
 }
